9617baaf4c6d97b6145981c49da8d82ac5f14991
jalvess021 <jalvess021@gmail.com>
2026-02-25 09:21:08 -0300

    Refactor (config) : reorganizacao e separacao de configuracao de aplicacao e banco

--- estoque/internal/config/config.go
+++ estoque/internal/config/config.go
@@ -1,6 +1,33 @@
 package config
 
+import (
+	"os"
+)
+
 type Config struct {
 	AppPort string
 	Database DatabaseConfig
+}
+
+func MustLoad() *Config {
+
+	cfg := &Config{
+
+		AppPort: os.Getenv("APP_PORT"),
+
+		Database: DatabaseConfig{
+
+			Driver: os.Getenv("DB_DRIVER"),
+
+			Host: os.Getenv("DB_HOST"),
+			Port: os.Getenv("DB_PORT"),
+			User: os.Getenv("DB_USER"),
+			Password: os.Getenv("DB_PASSWORD"),
+			Name: os.Getenv("DB_NAME"),
+		},
+	}
+
+	cfg.Database.Validate()
+
+	return cfg
 }
