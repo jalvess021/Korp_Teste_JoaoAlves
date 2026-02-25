db27859d629fbe3be449911f1af290ab8a4a29db
jalvess021 <jalvess021@gmail.com>
2026-02-25 15:23:44 -0300

    feat: refatorar o Bootstrap para criar a funcao NewApp e encapsular os componentes

--- estoque/cmd/main/main.go
+++ estoque/cmd/main/main.go
@@ -1,24 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/jalvess021/Korp_Teste_JoaoAlves/estoque/internal/bootstrap"
-	"github.com/jalvess021/Korp_Teste_JoaoAlves/estoque/internal/config"
-	"github.com/jalvess021/Korp_Teste_JoaoAlves/estoque/internal/infra/http"
 )
 
 func main() {
-	bootstrap.App()
-	router := http.SetupRouter()
 
-	port := config.Config{}.AppPort
-	if port == "" {
-		port = "5002"
+	app, err := bootstrap.NewApp()
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	serverAddr := fmt.Sprintf(":%s", port)
-	log.Printf("Servidor rodando na porta %s\n", port)
-	log.Fatal(router.Run(serverAddr))
+	defer app.DB.Close()
+
+	log.Printf("Servidor rodando na porta %s", app.Config.AppPort)
+
+	log.Fatal(
+		app.Router.Run(":" + app.Config.AppPort),
+	)
 }
--- estoque/internal/bootstrap/app.go
+++ estoque/internal/bootstrap/app.go
@@ -3,18 +3,32 @@ package bootstrap
 import (
 	"database/sql"
 
+	"github.com/gin-gonic/gin"
 	"github.com/jalvess021/Korp_Teste_JoaoAlves/estoque/internal/config"
 	"github.com/jalvess021/Korp_Teste_JoaoAlves/estoque/internal/database"
+	httpinfra "github.com/jalvess021/Korp_Teste_JoaoAlves/estoque/internal/infra/http"
 )
 
-func App() (*sql.DB, error) {
+type App struct {
+	Config *config.Config
+	DB     *sql.DB
+	Router *gin.Engine
+}
+
+func NewApp() (*App, error) {
+
 	cfg := config.MustLoad()
-	
+
 	db, err := database.ConnectPostgres(cfg.Database)
 	if err != nil {
 		return nil, err
 	}
-	defer db.Close()
 
-	return db, nil
+	router := httpinfra.SetupRouter(db)
+
+	return &App{
+		Config: cfg,
+		DB:     db,
+		Router: router,
+	}, nil
 }
